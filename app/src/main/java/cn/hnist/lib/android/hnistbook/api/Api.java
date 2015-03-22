88a5604effff48147a158ea202b7fb70c231496f
lujun <lujun_1993@foxmail.com>
2015-03-22 19:18:22 +0800

    add douban api

---
+++ app/src/main/java/cn/hnist/lib/android/hnistbook/api/Api.java
@@ -0,0 +1,11 @@
+package cn.hnist.lib.android.hnistbook.api;
+
+/**
+ * Created by lujun on 2015/3/22.
+ */
+public class Api {
+
+    private final static String DOUBAN_HOST = "https://api.douban.com/v2/book/";
+    public final static String BOOK_SEARCH_URL = DOUBAN_HOST + "search";
+    public final static String GET_ISBNBOOK_URL = DOUBAN_HOST + "isbn/";
+}
